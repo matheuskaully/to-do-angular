12b2c23316eb932f374e898c949942189b38104b
Matheus Kaúlly <kaully.santos@gmail.com>
2024-04-22 21:54:08 -0300

    feat: add animations provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideToastr(),
+    provideAnimations(),
   ]
 };
